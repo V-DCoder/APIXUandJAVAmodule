a9375c1d8437ae03ab004571c0d5d2e623ad7886
d-coder <varunp.karhadkar@gmail.com>
2019-06-19 02:44:54 +0530

    date formatter and formatting bug resolved

--- app/src/main/java/com/gojeck/apps/whertherly/adapter/ForecastAdapter.kt
+++ app/src/main/java/com/gojeck/apps/whertherly/adapter/ForecastAdapter.kt
@@ -4,13 +4,17 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.gojeck.apps.whertherly.R
 import com.gojeck.apps.whertherly.model.Forecastday
 import kotlinx.android.synthetic.main.list_item.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class ForecastAdapter : RecyclerView.Adapter<ForecastAdapter.ForecastViewHolder>() {
+    private val UNIX_JAVA_MULTIPLIER = 1000L
     var forecastday: List<Forecastday>? = null
+    var simpleDateFormat = SimpleDateFormat("EEEE", Locale.US)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastViewHolder {
         return ForecastViewHolder(
@@ -36,7 +40,10 @@ class ForecastAdapter : RecyclerView.Adapter<ForecastAdapter.ForecastViewHolder>
     inner class ForecastViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
         fun bindForecast(forecast: Forecastday) {
-            itemView.day.text = forecast.date
+
+            val cal = Calendar.getInstance()
+            cal.timeInMillis = forecast.dateEpoch * UNIX_JAVA_MULTIPLIER
+            itemView.day.text = simpleDateFormat.format(cal.time)
             itemView.temperature.text =
                 view.context.getString(R.string.celsius, forecast.day.avgtempC)
         }
